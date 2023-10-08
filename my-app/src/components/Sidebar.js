32b5bc120541e962e7304a2e42f819c859fa5dfb
Kate Jones <katejones@kates-air.lan>
2023-10-08 11:41:42 +0100

    Link component and css

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from "./css/Sidebar.module.css";
 
 function Sidebar() {
     return (
-        <div>
+        <div className = {css.sidebar}>
             <a href="#">My Photos</a>
             <a href="#">My Illustrations</a>
             <a href="#">My Paintings</a>
