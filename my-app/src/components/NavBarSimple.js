25dfd8ee789eaac20474462492b8f2de86f3898a
Kate Jones <katejones@kates-air.lan>
2023-10-08 13:40:41 +0100

    Class components challenge

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,33 @@
+import React from "react";
+import css from "./css/NavBarSimple.module.css";
+
+class NavBarSimple extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Hello, guest",
+            buttonText: "login",
+        };
+    }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            return {
+                message: prevState.message === "Hello, guest" ? "Welcome back, user" : "Hello, guest",
+                buttonText: prevState.buttonText === "login" ? "log out" : "login",
+            };
+        })
+    }
+
+    render() {
+        return (
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <span>{this.state.message}</span>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        );
+    }
+}
+
+export default NavBarSimple;
