6671e6b0e9c41234659e78f5d09f4846ad6b2a77
Kate Jones <katejones@kates-air.lan>
2023-10-08 11:37:39 +0100

    Complete first challenge

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div>
+            <a href="#">My Photos</a>
+            <a href="#">My Illustrations</a>
+            <a href="#">My Paintings</a>
+        </div>
+    )
+}
+
+export default Sidebar;
